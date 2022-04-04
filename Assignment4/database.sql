4f021d3a081755de074e5f70af186e94236ca7f1
Alejandro Gonzalez <44660767+gonzaale@users.noreply.github.com>
2022-04-03 21:46:04 -0500

    Provide database schema SQL

---
+++ Assignment4/database.sql
@@ -0,0 +1,144 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "fuel_history" (
+	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	"user_id"	TEXT NOT NULL,
+	"gallons"	REAL NOT NULL,
+	"address"	TEXT NOT NULL,
+	"date"	datetime NOT NULL,
+	"suggested_price"	REAL NOT NULL,
+	"total_due"	REAL NOT NULL,
+	FOREIGN KEY("user_id") REFERENCES "auth_user"("id") ON UPDATE CASCADE
+);
+CREATE TABLE IF NOT EXISTS "company" (
+	"profit_margin"	REAL NOT NULL,
+	PRIMARY KEY("profit_margin")
+);
+CREATE TABLE IF NOT EXISTS "auth_user" (
+	"id"	integer NOT NULL,
+	"password"	varchar(128) NOT NULL,
+	"last_login"	datetime,
+	"is_superuser"	bool NOT NULL,
+	"username"	varchar(150) NOT NULL UNIQUE,
+	"last_name"	varchar(150) NOT NULL,
+	"email"	varchar(254) NOT NULL,
+	"is_staff"	bool NOT NULL,
+	"is_active"	bool NOT NULL,
+	"date_joined"	datetime NOT NULL,
+	"first_name"	varchar(150) NOT NULL,
+	"birthdate"	datetime NOT NULL,
+	"state"	varchar(20) NOT NULL,
+	"fuel_quote"	REAL NOT NULL,
+	PRIMARY KEY("id")
+);
+CREATE TABLE IF NOT EXISTS "django_session" (
+	"session_key"	varchar(40) NOT NULL,
+	"session_data"	text NOT NULL,
+	"expire_date"	datetime NOT NULL,
+	PRIMARY KEY("session_key")
+);
+CREATE TABLE IF NOT EXISTS "auth_group" (
+	"id"	integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+	"name"	varchar(150) NOT NULL UNIQUE
+);
+CREATE TABLE IF NOT EXISTS "auth_permission" (
+	"id"	integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+	"content_type_id"	integer NOT NULL,
+	"codename"	varchar(100) NOT NULL,
+	"name"	varchar(255) NOT NULL,
+	FOREIGN KEY("content_type_id") REFERENCES "django_content_type"("id") DEFERRABLE INITIALLY DEFERRED
+);
+CREATE TABLE IF NOT EXISTS "django_content_type" (
+	"id"	integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+	"app_label"	varchar(100) NOT NULL,
+	"model"	varchar(100) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "django_admin_log" (
+	"id"	integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+	"action_time"	datetime NOT NULL,
+	"object_id"	text,
+	"object_repr"	varchar(200) NOT NULL,
+	"change_message"	text NOT NULL,
+	"content_type_id"	integer,
+	"user_id"	integer NOT NULL,
+	"action_flag"	smallint unsigned NOT NULL CHECK("action_flag">=0),
+	FOREIGN KEY("user_id") REFERENCES "auth_user"("id") DEFERRABLE INITIALLY DEFERRED,
+	FOREIGN KEY("content_type_id") REFERENCES "django_content_type"("id") DEFERRABLE INITIALLY DEFERRED
+);
+CREATE TABLE IF NOT EXISTS "auth_user_user_permissions" (
+	"id"	integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+	"user_id"	integer NOT NULL,
+	"permission_id"	integer NOT NULL,
+	FOREIGN KEY("permission_id") REFERENCES "auth_permission"("id") DEFERRABLE INITIALLY DEFERRED,
+	FOREIGN KEY("user_id") REFERENCES "auth_user"("id") DEFERRABLE INITIALLY DEFERRED
+);
+CREATE TABLE IF NOT EXISTS "auth_user_groups" (
+	"id"	integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+	"user_id"	integer NOT NULL,
+	"group_id"	integer NOT NULL,
+	FOREIGN KEY("group_id") REFERENCES "auth_group"("id") DEFERRABLE INITIALLY DEFERRED,
+	FOREIGN KEY("user_id") REFERENCES "auth_user"("id") DEFERRABLE INITIALLY DEFERRED
+);
+CREATE TABLE IF NOT EXISTS "auth_group_permissions" (
+	"id"	integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+	"group_id"	integer NOT NULL,
+	"permission_id"	integer NOT NULL,
+	FOREIGN KEY("permission_id") REFERENCES "auth_permission"("id") DEFERRABLE INITIALLY DEFERRED,
+	FOREIGN KEY("group_id") REFERENCES "auth_group"("id") DEFERRABLE INITIALLY DEFERRED
+);
+CREATE TABLE IF NOT EXISTS "django_migrations" (
+	"id"	integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+	"app"	varchar(255) NOT NULL,
+	"name"	varchar(255) NOT NULL,
+	"applied"	datetime NOT NULL
+);
+CREATE INDEX IF NOT EXISTS "django_session_expire_date_a5c62663" ON "django_session" (
+	"expire_date"
+);
+CREATE INDEX IF NOT EXISTS "auth_permission_content_type_id_2f476e4b" ON "auth_permission" (
+	"content_type_id"
+);
+CREATE UNIQUE INDEX IF NOT EXISTS "auth_permission_content_type_id_codename_01ab375a_uniq" ON "auth_permission" (
+	"content_type_id",
+	"codename"
+);
+CREATE UNIQUE INDEX IF NOT EXISTS "django_content_type_app_label_model_76bd3d3b_uniq" ON "django_content_type" (
+	"app_label",
+	"model"
+);
+CREATE INDEX IF NOT EXISTS "django_admin_log_user_id_c564eba6" ON "django_admin_log" (
+	"user_id"
+);
+CREATE INDEX IF NOT EXISTS "django_admin_log_content_type_id_c4bce8eb" ON "django_admin_log" (
+	"content_type_id"
+);
+CREATE INDEX IF NOT EXISTS "auth_user_user_permissions_permission_id_1fbb5f2c" ON "auth_user_user_permissions" (
+	"permission_id"
+);
+CREATE INDEX IF NOT EXISTS "auth_user_user_permissions_user_id_a95ead1b" ON "auth_user_user_permissions" (
+	"user_id"
+);
+CREATE UNIQUE INDEX IF NOT EXISTS "auth_user_user_permissions_user_id_permission_id_14a6b632_uniq" ON "auth_user_user_permissions" (
+	"user_id",
+	"permission_id"
+);
+CREATE INDEX IF NOT EXISTS "auth_user_groups_group_id_97559544" ON "auth_user_groups" (
+	"group_id"
+);
+CREATE INDEX IF NOT EXISTS "auth_user_groups_user_id_6a12ed8b" ON "auth_user_groups" (
+	"user_id"
+);
+CREATE UNIQUE INDEX IF NOT EXISTS "auth_user_groups_user_id_group_id_94350c0c_uniq" ON "auth_user_groups" (
+	"user_id",
+	"group_id"
+);
+CREATE INDEX IF NOT EXISTS "auth_group_permissions_permission_id_84c5c92e" ON "auth_group_permissions" (
+	"permission_id"
+);
+CREATE INDEX IF NOT EXISTS "auth_group_permissions_group_id_b120cbf9" ON "auth_group_permissions" (
+	"group_id"
+);
+CREATE UNIQUE INDEX IF NOT EXISTS "auth_group_permissions_group_id_permission_id_0cd325b0_uniq" ON "auth_group_permissions" (
+	"group_id",
+	"permission_id"
+);
+COMMIT;
